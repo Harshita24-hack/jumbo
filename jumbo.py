4f40d52831fd4b7b9b810558233cf551d7df0ab1
Harshita24-hack <harshitasharma9007979663@gmail.com>
2025-09-10 17:16:47 +0530

    Update jumbo.py

--- jumbo.py
+++ jumbo.py
@@ -2,10 +2,10 @@
 import random
 
 words = {
-    3: ["cat", "dog", "sun"],
-    4: ["bird", "code", "game"],
-    5: ["apple", "basic", "cloud"],
-    6: ["banana", "coffee", "fables"]
+    3: ["cat", "dog", "sun","Map","Box","Pen","Sky","Ice","Fun"],
+    4: ["bird", "code", "game","Love", "Tree", "Book", "Star", "Home", "Fire", "Wind", "Rain", "Hope"],
+    5: ["apple", "basic", "cloud","Smile", "Dream", "Light", "Peace", "Heart", "Bread", "Laugh", "Music", "Plant"],
+    6: ["banana", "coffee", "fables","Friend", "Nature", "Beauty", "Castle", "Spirit", "Animal", "Bright", "Silver", "Flower", "Future"]
 }
 
 def jumble_word(word):
@@ -22,3 +22,4 @@ if answer.lower() == word:
     print("Correct!")
 else:
     print(f"Sorry, correct answer was {word}.")
+
